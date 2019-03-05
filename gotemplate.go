b397c40b57dc5debaf188e124a02a0c79c830d2a
itswcg <itswcg@gmail.com>
2019-03-05 21:07:10 +0800

    feat: hello,world

---
+++ gotemplate.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"fmt"
+)
+
+const c = "C"
+
+//var v int = 5
+
+type T struct{}
+
+func init() {}
+
+func main() {
+	var a int
+	//func1()
+
+	fmt.Println(a)
+}
+
+func (t T) Method1() {}
+
+func Func1() {}
--- helloworld.go
+++ helloworld.go
@@ -1,5 +1,25 @@
-package main
+package main // 指明文件属于哪个包， 根据惯例，每个目录只包含一个包
+
+import fm "fmt"
+
+type (
+	IZ  int
+	FZ  float64
+	STR string
+)
 
 func main() {
-	println("Hello, world")
+	fm.Println("Hello, world")
 }
+
+//var a iZ = 5
+
+func Sum(a, b int) int {
+	return a + b
+}
+
+// go的文件命名均由小写字母组成
+// 类型 int float bool string 结构化的： struct, array, slice, map, channel, interface
+// 结构化的类型没有真正的值，可以用nil作为默认值
+// func FunctionName(a typea, b typeb) (t1 type1, t2 type2)
+// 类型转换 a := 5.0 b := int(a)
